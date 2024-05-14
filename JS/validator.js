237e69afa90455db915309128e9b2657338ec9bc
hect1x <christian.sdkn@gmail.com>
2024-05-14 22:49:02 +0700

    finish

--- JS/validator.js
+++ JS/validator.js
@@ -77,3 +77,60 @@ function validateRegForm(){
 
     return true;
 }
+
+function validateEditForm(){
+    const usernameinp = document.getElementsByName('username')[0];
+    const passwordinp = document.getElementsByName('password')[0];
+    const emailinp = document.getElementsByName('email')[0];
+    const conpasswordinp = document.getElementsByName('conpassword')[0];
+    const numberinp = document.getElementsByName('number')[0];
+    let messages = [];
+
+    const username = usernameinp.value.trim();
+    const password = passwordinp.value.trim();
+    const email = emailinp.value.trim();
+    const conpassword = conpasswordinp.value.trim();
+    const number = numberinp.value.trim();
+    if(username === "") {
+        alert('Username is Required and cannot be blank');
+        return false;
+    }
+    if(username.length < 5 || username.length > 15) {
+        alert('Username must be between 5-15 characters');
+        return false;
+    }     
+    if(email === "") {
+        alert('Email is Required and cannot be blank');
+        return false;
+    }   
+    if(!email.endsWith("@binus.ac.id")) {
+        alert('Email must end with "@binus.ac.id"');
+        return false;
+    }
+    if(!number){
+        alert('Phone Number is Required and cannot be blank');
+        return false;
+    }
+    if(password === ""){
+        alert('Password is Required and cannot be blank');
+        return false;
+    }
+    if(password.length < 5 || password.length > 30) {
+        alert('Password must be between 5-30 characters');
+        return false;
+    }
+    if (!password.match(/[a-z]/) || !password.match(/[A-Z]/) || !password.match(/[0-9]/)) {
+        alert('Password must contain both upper and lowercase letters and at least one number');
+        return false;
+    }
+    if(conpassword === "") {
+        alert('Confirm Password is Required and cannot be blank');
+        return false;
+    }
+    if(password !== conpassword) {
+        alert('Passwords do not match');
+        return false;
+    }
+
+    return true;
+}
